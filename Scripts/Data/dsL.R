9d70f3de93d153b7ea7084b1aedc949fb19f1eae
ampiccinin <piccinin@uvic.ca>
2014-11-07 14:59:51 -0800

    added , all=TRUE
    this keeps all cases in both files, even if an individual does not have
    data in both files. You can delete it if you only want to keep
    individuals with both sets of data (which I think is what you want).

--- Scripts/Data/dsL.R
+++ Scripts/Data/dsL.R
@@ -9,7 +9,8 @@ ds01 <- da25281.0001
 ds02 <- da29282.0001
 str(ds01)
 
-ds0 <- merge(ds01,ds02,by="M2ID")
+ds0 <- merge(ds01,ds02,by="M2ID", all=TRUE)
+
 
 myvars <- c("v1", "v2", "v3",...)
 ds0Core <- mydata[myvars]
