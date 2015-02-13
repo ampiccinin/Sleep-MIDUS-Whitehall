95a51c2d5139f929e86c8b1a0e4ab4e557930abd
janynm <janyn.mercado@gmail.com>
2015-02-13 02:04:20 -0800

    Grouped daily sleep variables and added ds00
    Grouped daily sleep variables and dropped them to create ds00 (all
    variables of interest excluding daily sleep variables)

--- Scripts/Data/dsL.R
+++ Scripts/Data/dsL.R
@@ -68,6 +68,17 @@ MIDUS_all <- c('M2ID', 'B1PAGE_M2.x', 'B1PGENDER.x', 'B4ZAGE',
                'B4AD115',  'B4AD215',  'B4AD315',  'B4AD415',  'B4AD515',  'B4AD615',  'B4AD715', 
                'B4AD115A', 'B4AD215A', 'B4AD315A', 'B4AD415A', 'B4AD515A', 'B4AD615A', 'B4AD715A', 'B3TCOMPZ3', 'B3TEMZ3', 'B3TEFZ3')
 
+#object with MIDUS daily sleep variables
+MIDUS_sleepdaily <- c(
+  'B4AD17',    'B4AD27',    'B4AD37',    'B4AD47',    'B4AD57',    'B4AD67',    'B4AD77',  
+'B4AD110',   'B4AD210',   'B4AD310',   'B4AD410',   'B4AD510',   'B4AD610',   'B4AD710',  
+'B4AD113',   'B4AD213',   'B4AD313',   'B4AD413',   'B4AD513',   'B4AD613',   'B4AD713',  
+'B4AD117',   'B4AD217',   'B4AD317',   'B4AD417',   'B4AD517',   'B4AD617',   'B4AD717',   
+'B4AD120',  'B4AD220',  'B4AD320',  'B4AD420',  'B4AD520',  'B4AD620',  'B4AD720',  
+'B4AD118',  'B4AD218',  'B4AD318',  'B4AD418',  'B4AD518',  'B4AD618',  'B4AD718', 
+'B4AD115',  'B4AD215',  'B4AD315',  'B4AD415',  'B4AD515',  'B4AD615',  'B4AD715', 
+'B4AD115A', 'B4AD215A', 'B4AD315A', 'B4AD415A', 'B4AD515A', 'B4AD615A', 'B4AD715A')
+
 # MIDUS_pvars codes:
 # M2ID = Participant ID number
 # B1PAGE_M2 = Age deterimined by subtracting DOB_Final from b1ipdate
@@ -104,4 +115,18 @@ dscontrol <- ds03[MIDUS_controls]
 
 ds0 <- merge(ds_ALL,dscontrol,by="M2ID")
 
-describe(ds_ALL)
+
+#ds0 with daily sleep variables dropped
+MIDUS_sleepdaily <- names(ds0) %in% c(
+  'B4AD17',    'B4AD27',    'B4AD37',    'B4AD47',    'B4AD57',    'B4AD67',    'B4AD77',  
+  'B4AD110',   'B4AD210',   'B4AD310',   'B4AD410',   'B4AD510',   'B4AD610',   'B4AD710',  
+  'B4AD113',   'B4AD213',   'B4AD313',   'B4AD413',   'B4AD513',   'B4AD613',   'B4AD713',  
+  'B4AD117',   'B4AD217',   'B4AD317',   'B4AD417',   'B4AD517',   'B4AD617',   'B4AD717',   
+  'B4AD120',  'B4AD220',  'B4AD320',  'B4AD420',  'B4AD520',  'B4AD620',  'B4AD720',  
+  'B4AD118',  'B4AD218',  'B4AD318',  'B4AD418',  'B4AD518',  'B4AD618',  'B4AD718', 
+  'B4AD115',  'B4AD215',  'B4AD315',  'B4AD415',  'B4AD515',  'B4AD615',  'B4AD715', 
+  'B4AD115A', 'B4AD215A', 'B4AD315A', 'B4AD415A', 'B4AD515A', 'B4AD615A', 'B4AD715A')
+ds00 <- ds0[!MIDUS_sleepdaily]
+
+#descriptive stats on ds0
+describe(ds0)
